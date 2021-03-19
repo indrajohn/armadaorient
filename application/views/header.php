256ceb0fe9ff5785a41a24356208574bef7b7b0a
indrajohn <indrajohn2@gmail.com>
2021-03-19 12:05:55 +0700

    add analytic

--- application/views/header.php
+++ application/views/header.php
@@ -5,35 +5,47 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html class="no-js" lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Armada Orient | Furniture</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>Armada Orient | Furniture</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Place favicon.ico in the root directory -->
-    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico">
-    <link rel="apple-touch-icon" href="assets/apple-touch-icon.png">
+	<!-- Place favicon.ico in the root directory -->
+	<link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico">
+	<link rel="apple-touch-icon" href="assets/apple-touch-icon.png">
 
 
-    <!-- All css files are included here. -->
-    <!-- Bootstrap fremwork main css -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <!-- Owl Carousel min css -->
-    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
-    <!-- This core.css file contents all plugings css file. -->
-    <link rel="stylesheet" href="assets/css/core.css">
-    <!-- Theme shortcodes/elements style -->
-    <link rel="stylesheet" href="assets/css/shortcode/shortcodes.css">
-    <!-- Theme main style -->
-    <link rel="stylesheet" href="assets/style.css">
-    <!-- Responsive css -->
-    <link rel="stylesheet" href="assets/css/responsive.css">
-    <!-- User style -->
-    <link rel="stylesheet" href="assets/css/custom.css">
-    <!-- Modernizr JS -->
-    <script src="assets/js/vendor/modernizr-3.5.0.min.js"></script>
-    <script data-ad-client="ca-pub-9104407523380318" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    
+	<!-- All css files are included here. -->
+	<!-- Bootstrap fremwork main css -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<!-- Owl Carousel min css -->
+	<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+	<!-- This core.css file contents all plugings css file. -->
+	<link rel="stylesheet" href="assets/css/core.css">
+	<!-- Theme shortcodes/elements style -->
+	<link rel="stylesheet" href="assets/css/shortcode/shortcodes.css">
+	<!-- Theme main style -->
+	<link rel="stylesheet" href="assets/style.css">
+	<!-- Responsive css -->
+	<link rel="stylesheet" href="assets/css/responsive.css">
+	<!-- User style -->
+	<link rel="stylesheet" href="assets/css/custom.css">
+	<!-- Modernizr JS -->
+	<script src="assets/js/vendor/modernizr-3.5.0.min.js"></script>
+	<script data-ad-client="ca-pub-9104407523380318" async
+		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZXNSKKYWC9"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-ZXNSKKYWC9');
+
+	</script>
 </head>
